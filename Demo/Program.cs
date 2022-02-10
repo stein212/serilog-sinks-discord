0c57ff7a96cfd5bffe0d5dd733ed940555f83634
stein212 <qscaxz0@gmail.com>
2022-02-10 19:28:56 +0800

    Update demo project

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,42 +2,64 @@
 using Serilog;
 using Serilog.Sinks.Discord;
 
-// https://discord.com/api/webhooks/941016909887983616/WT_XgmKkN10bJ8q4mpW1Q3NmUDjABBNEjXJ1gEQteX17GTZ4hH_8CwC5V_yn8jx_zywe
+// SET THE ENVIROMENT VARIABLES BEFORE RUNNING THE DEMO
+// See https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks on how to get the webhook id and token
+// Format: https://discord.com/api/webhooks/<WEBHOOk_ID>/<WEBHOOK_TOKEN>
 var webhookId = ulong.Parse(Environment.GetEnvironmentVariable("WEBHOOK_ID")!);
-var webhookToken = Environment.GetEnvironmentVariable("WEBHOOK_TOKEN");
+var webhookToken = Environment.GetEnvironmentVariable("WEBHOOK_TOKEN")!;
 Log.Logger = new LoggerConfiguration()
     // use async wrapper to log and forget (no need to wait for discord to received the log)
-    .WriteTo.Async(a => a.Discord(webhookId, webhookToken!))
+    .WriteTo.Console()
+    .WriteTo.Async(a => a.Discord(webhookId, webhookToken))
+    .MinimumLevel.Verbose()
     .CreateLogger();
 
-var token = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890 !@#$%^&*()_+{}:l',./<>?~`\n";
-
-var testMultipleEmbedMessageStringBuilder = new StringBuilder();
-
-for (var i = 0; i < 100; ++i)
-    testMultipleEmbedMessageStringBuilder.Append(token);
-
-Console.WriteLine($"Sending message with size: {testMultipleEmbedMessageStringBuilder.Length}");
-
-var testMultipleEmbedMessage = testMultipleEmbedMessageStringBuilder.ToString();
-
-// try
-// {
-//     throw new Exception("Test logging exception");
-// }
-// catch (Exception e)
-// {
-//     Log.Fatal(e, testMultipleEmbedMessage);
-// }
+/* Hello world */
+Log.Information("Hello, world!");
 
 
-// test rapid
-// should log very fast but discord will get it slowly
+/* Different log levels */
+Log.Verbose("This is a Verbose message");
+Log.Debug("This is a Debug message");
+Log.Information("This is an Information message");
+Log.Warning("This is a Warning message");
+Log.Error("This is an Error message");
+Log.Fatal("This is a Fatal message");
+
+
+/* Example exception with stacktrce */
+try
+{
+    throw new Exception("Example exception thrown");
+}
+catch (Exception e)
+{
+    Log.Error(e, "This is an Error message with an Exception passed in");
+}
+
+
+/* Async wrapper behaviour */
+/* should log very fast on console while discord will get it later */
 for (int i = 0; i < 5; ++i)
 {
     Log.Information($"{i}");
-    Console.WriteLine(i);
 }
+
+
+/* Long message example */
+var token = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890 !@#$%^&*()_+{}:l',./<>?~`\n";
+
+var longMessageBuilder = new StringBuilder();
+
+for (var i = 0; i < 100; ++i)
+    longMessageBuilder.Append(token);
+
+Console.WriteLine($"Sending message with size: {longMessageBuilder.Length}");
+
+var longMessage = longMessageBuilder.ToString();
+
+Log.Information(longMessage);
+
 
 // Flush all then end program
 Log.CloseAndFlush();
